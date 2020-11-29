d1b287a96bbde466e62327126b77588f89d40783
Dmitriy Dudnik <63814431+dmdudnik@users.noreply.github.com>
2020-11-29 18:39:33 +0200

    исправлено имя переменной

--- Js/task-4.js
+++ Js/task-4.js
@@ -1,5 +1,5 @@
 const PRICE_PER_DROID = 3000;
-const CREDITS = 23580;
+const credits = 23580;
 let numDroid;
 let totalPrice = 0;
 let сreditsLeft = 0;
@@ -10,10 +10,10 @@ numDroid = prompt('Введите количество дроидов');
 if (numDroid !== null) {
   numDroid = Number(numDroid);
   totalPrice = PRICE_PER_DROID * numDroid;
-  сreditsLeft = CREDITS - totalPrice;
+  сreditsLeft = credits - totalPrice;
 
   message =
-    totalPrice <= CREDITS
+    totalPrice <= credits
       ? `Вы купили ${numDroid} дроидов, на счету осталось ${сreditsLeft} кредитов.`
       : 'Недостаточно средств на счету!';
 } else {
